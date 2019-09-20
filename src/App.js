fbfa81c0f30da27bb0875475d11ee4ae1f5a8d48
unknown <you@example.com>
2019-09-20 09:52:25 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTask: "helli DD..",
+    }
+  }
+
+  handleTaskChange = (event) => {
+    this.setState({
+      newTask: event.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">Hello world <span aria-label="emoji" role="img">🔥</span></h1>
+        <input onChange={this.handleTaskChange} type="text" className="new-task"/>
+        <h2 className="test-label">{this.state.newTask}</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
